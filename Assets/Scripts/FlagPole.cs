91e6072e5840ecc74d81e7e8aad1d21c60244c55
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2024-05-10 21:42:50 +0300

    Added assests & level 1-3 (cantina)

--- Assets/Scripts/FlagPole.cs
+++ Assets/Scripts/FlagPole.cs
@@ -24,8 +24,8 @@ public class FlagPole : MonoBehaviour
         player.GetComponent<PlayerMovement>().enabled = false;
 
         yield return MoveTo(player, poleBottom.position);
-        yield return MoveTo(player, player.position + Vector3.right);
-        yield return MoveTo(player, player.position + Vector3.right + Vector3.down);
+        // yield return MoveTo(player, player.position + Vector3.right);
+        // yield return MoveTo(player, player.position + Vector3.right + Vector3.down);
         yield return MoveTo(player, castle.position);
 
         player.gameObject.SetActive(false);
